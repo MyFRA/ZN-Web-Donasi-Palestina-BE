20e451c470738a45ebebaa48e7d98270f6dd5b07
MyFRA <tomyntapss@gmail.com>
2025-01-18 00:04:43 +0700

    add force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
         Paginator::useBootstrap();
     }
 }
